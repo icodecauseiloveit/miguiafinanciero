521529f6535bfe7c40a38d9b3ab80211231bafda
Gary <gbaldrich16@gmail.com>
2026-04-04 21:37:49 -0500

    chore: fix domain from .co to .com in sitemap and robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://miguiafinanciero.co/sitemap.xml',
+    sitemap: 'https://miguiafinanciero.com/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://miguiafinanciero.co'
+  const baseUrl = 'https://miguiafinanciero.com'
 
   return [
     {
