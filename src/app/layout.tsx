757a6ba89d2d3f162cd6beedb7739462e9df50f9
Gary <gbaldrich16@gmail.com>
2026-06-30 14:31:15 -0500

    chore: update GTM ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-P2K9T365');
+            })(window,document,'script','dataLayer','GTM-KPQGMHDF');
           `}
         </Script>
       </head>
@@ -49,7 +49,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P2K9T365"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KPQGMHDF"
             height="0" 
             width="0" 
             style={{ display: "none", visibility: "hidden" }}
